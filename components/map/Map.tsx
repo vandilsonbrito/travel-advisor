966694683b836826997afba5de4243f3f7935d36
vandilsonbrito <vandilson25@outlook.com>
2024-08-29 11:59:10 -0300

    Feat: add Marker prop on the map on every restaurant data location from API

--- components/map/Map.tsx
+++ components/map/Map.tsx
@@ -1,18 +1,13 @@
 'use client';
-import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
-import React from 'react';
+import { GoogleMap, useJsApiLoader, OverlayView, Marker } from '@react-google-maps/api';
+import React, { useEffect, useState } from 'react';
 import { useAtom } from 'jotai';
-import { mapGeoBounds, refecthNearbyList } from '@/utils/stores/atoms';
+import { mapGeoBounds, refecthNearbyList, restaurantsAPIData } from '@/utils/stores/atoms';
 
 
-const containerStyle = {
+  const containerStyle = {
     width: '100dwh',
     height: 'calc(100dvh - 90px)'
-  };
-  
-  const center = { //Criar opção de Geolocator do web.navigator para ver resultados próximo do usuário
-    lat: -23.539507115902488,
-    lng: -46.63220789819972
   };
 
   function Map() {
@@ -41,23 +36,26 @@ const containerStyle = {
           console.log(restaurantsData)
       }, [restaurantsData])
 
-    const GOOGLE_MAPS_KEY = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || '';
-    const { isLoaded } = useJsApiLoader({
-        id: 'google-map-script',
-        googleMapsApiKey: GOOGLE_MAPS_KEY
+      const GOOGLE_MAPS_KEY = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || '';
+      const { isLoaded } = useJsApiLoader({
+          id: 'google-map-script',
+          googleMapsApiKey: GOOGLE_MAPS_KEY
       })
-    
+      
       const [map, setMap] = React.useState<google.maps.Map | null>(null);
       const mapRef = React.useRef<google.maps.Map | null>(null);
     
+      /* Carrega o mapa e seta o zoom inicial */
       const onLoad = React.useCallback(function callback(map: google.maps.Map) {
-        // This is just an example of getting and using the map instance!!! don't just blindly copy!
         mapRef.current = map;
-        const bounds = new window.google.maps.LatLngBounds(center);
-        map.fitBounds(bounds);
-    
-        setMap(map)
-      }, [])
+        const bounds = new window.google.maps.LatLngBounds(usersLocation);
+        setTimeout(() => {
+          map.fitBounds(bounds);
+          const desiredZoom = 17;
+          map.setZoom(desiredZoom);
+          setMap(map);
+        }, 500);
+      }, [usersLocation])
     
       const onUnmount = React.useCallback(function callback(map: google.maps.Map) {
         setMap(null)
@@ -98,30 +96,40 @@ const containerStyle = {
           }, 500);
           return () => clearTimeout(timeoutSetRefetch);
         }
-      };
-      
+      }, [refetchList])
 
-    return (
-        <section className="w-full h-full bg-slate-100" style={{ height: 'calc(100dvh - 90px)' }}>
-            {
-                isLoaded ? (
-                    <GoogleMap
-                        mapContainerStyle={containerStyle}
-                        center={center}
-                        zoom={3}
-                        onLoad={onLoad}
-                        onUnmount={onUnmount}
-                        onBoundsChanged={handleBoundsChanged}
-                    >
-                        { /* Child components, such as markers, info windows, etc. */ }
-                        <></>
-                    </GoogleMap>
+      return (
+        <section className="w-full h-full relative bg-slate-100" style={{ height: 'calc(100vh - 90px)' }}>
+            {isLoaded ? (
+              <GoogleMap
+                mapContainerStyle={containerStyle}
+                center={usersLocation}
+                zoom={10}
+                onLoad={onLoad}
+                onUnmount={onUnmount}
+                onBoundsChanged={handleBoundsChanged}
+              >
+                {restaurantsData ? (
+                  restaurantsData.map((restaurant, index) => (
+                    <Marker
+                      key={index}
+                      position={{
+                        lat: parseFloat(restaurant.lat),
+                        lng: parseFloat(restaurant.lon)
+                      }}
+                      
+                    />
+                  ))
                 ) : 
-                <> </>
-            }
-
-        </section>
-    )
-}
+                (
+                  <></>
+                )}
+              </GoogleMap>
+            ) : (
+              <></>
+            )}
+      </section>
+      )
+  }
 
 export default React.memo(Map)
