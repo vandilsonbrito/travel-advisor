e25ac28e26799b557e14a45734dc9354f0f078d3
vandilsonbrito <vandilson25@outlook.com>
2024-08-29 12:02:05 -0300

    Feat: add code to display restaurant names on aside tag

--- components/listofoccurrences/ListOfOccurrences.tsx
+++ components/listofoccurrences/ListOfOccurrences.tsx
@@ -1,27 +1,61 @@
 'use client';
 import { useNearbyList } from "@/hooks/useNearbyList";
-import { refecthNearbyList } from "@/utils/stores/atoms";
+import { refecthNearbyList, restaurantsAPIData } from "@/utils/stores/atoms";
 import { useAtom } from "jotai";
 import { useEffect } from "react";
+
 
 function ListOfOccurrences() {
 
   const [refecthList, setRefetchList] = useAtom(refecthNearbyList);
-  const { data, refetch } = useNearbyList();
+  const [restaurantsData, setRestaurantsData] = useAtom(restaurantsAPIData);
+  const { data, refetch, isFetching } = useNearbyList();
   useEffect(() => {
-    console.log("Data", data);
-    setRefetchList(false);
-  }, [data, setRefetchList])
+    setRestaurantsData(data?.elements);
+    console.log("Data", data?.elements);
+    console.log("Atualizou Data")
+  }, [data, refecthList, setRestaurantsData])
+
+  console.log("isFetching", isFetching)
 
   useEffect(() => {
     if(refecthList) {
       refetch();
+      console.log("------------------Deu Refetching!!!")
     }
-  }, [refecthList, refetch])
+  }, [refecthList])
 
   return (
-    <aside className="w-[20rem] h-full bg-slate-300" style={{ height: 'calc(100dvh - 90px)' }}>
-
+    <aside className="w-[20rem] h-full shadow-xl rounded-md flex flex-col items-center gap-3 p-3 overflow-y-scroll scroll-smooth" style={{ height: 'calc(100dvh - 90px)' }}>
+        <>
+            {
+                restaurantsData ? (
+                    restaurantsData.map((restaurant, index) => 
+                    ( 
+                        <>
+                          {
+                            restaurant.tags.name ? 
+                            (
+                              <div className="w-full h-full max-h-[4rem] bg-slate-50 rounded-md flex flex-col justify-center items-center p-3">
+                                  <p className="text-sm">{restaurant.tags.name}</p>
+                              </div>
+                            ) 
+                            : 
+                            (
+                              <>
+                              </>
+                            )
+                          }
+                        </>
+                        
+                    ))
+                ) 
+                : 
+                (
+                    <p>No data available, please refresh the page</p>
+                )
+            }
+        </>
     </aside>
   )
 }
